41bf084134f924cdad8d966ad4c44438b89d91d8
yang4978 <40636563+yang4978@users.noreply.github.com>
2019-04-15 15:01:46 +0800

    Create 3_tuples.py

---
+++ Python/Basic Data Types/3_tuples.py
@@ -0,0 +1,8 @@
+#https://www.hackerrank.com/challenges/python-tuples/problem
+
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = map(int, input().split())
+    tup = tuple(integer_list);
+    print (hash(tup));
+
