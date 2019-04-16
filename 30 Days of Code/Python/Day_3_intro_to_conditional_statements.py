844a4cd7c2b04453a347ddd97ea2b30a59356ad4
yang4978 <40636563+yang4978@users.noreply.github.com>
2019-04-16 09:43:08 +0800

    Create Day_3_intro_to_conditional_statements.py

---
+++ 30 Days of Code/Python/Day_3_intro_to_conditional_statements.py
@@ -0,0 +1,15 @@
+#https://www.hackerrank.com/challenges/30-conditional-statements/problem
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    N = int(input())
+    if (N%2==1) or (6<=N<=20):
+        print ("Weird");
+    else:
+        print ("Not Weird");
