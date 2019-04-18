be7d12f1b9bb887548c5bdfffa590d5616b0ff85
yang4978 <40636563+yang4978@users.noreply.github.com>
2019-04-18 20:23:14 +0800

    Create 9_set.symmetric_difference().py

---
+++ Python/Sets/9_set.symmetric_difference().py
@@ -0,0 +1,7 @@
+#https://www.hackerrank.com/challenges/py-set-symmetric-difference-operation/problem
+
+n = int(input());
+s1 = set(map(int,input().split()));
+m = int(input());
+s2 = set(map(int,input().split()));
+print (len(s1.symmetric_difference(s2)));
