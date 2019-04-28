0b41dcc2a976685e64ac6f29de53bfb3e3db50fa
yang4978 <40636563+yang4978@users.noreply.github.com>
2019-04-28 15:01:26 +0800

    Create 01_classes:dealing_with_complex_numbesr.py

---
+++ Python/Classes/01_classes:dealing_with_complex_numbesr.py
@@ -0,0 +1,47 @@
+#https://www.hackerrank.com/challenges/class-1-dealing-with-complex-numbers/problem
+
+import math
+
+class Complex(object):
+    def __init__(self, real, imaginary):
+        self.real = real;
+        self.imaginary = imaginary;
+        
+    def __add__(self, no):
+        temp_real = self.real + no.real;
+        temp_imaginary = self.imaginary + no.imaginary
+        return Complex(temp_real,temp_imaginary)
+        
+    def __sub__(self, no):
+        temp_real = self.real - no.real;
+        temp_imaginary = self.imaginary - no.imaginary
+        return Complex(temp_real,temp_imaginary)
+        
+    def __mul__(self, no):
+        temp_real = self.real * no.real - self.imaginary * no.imaginary;
+        temp_imaginary = self.imaginary * no.real +self.real * no.imaginary;
+        return Complex(temp_real,temp_imaginary)
+
+    def __truediv__(self, no):
+        temp_real = (self.real * no.real + self.imaginary * no.imaginary)/(no.real ** 2 + no.imaginary ** 2)
+        temp_imaginary = (self.imaginary * no.real - self.real * no.imaginary)/(no.real ** 2 + no.imaginary ** 2)
+        return Complex(temp_real,temp_imaginary)
+
+    def mod(self):
+        return Complex(math.sqrt(self.real ** 2 + self.imaginary ** 2) , 0)
+
+    def __str__(self):
+        if self.imaginary == 0:
+            result = "%.2f+0.00i" % (self.real)
+        elif self.real == 0:
+            if self.imaginary >= 0:
+                result = "0.00+%.2fi" % (self.imaginary)
+            else:
+                result = "0.00-%.2fi" % (abs(self.imaginary))
+        elif self.imaginary > 0:
+            result = "%.2f+%.2fi" % (self.real, self.imaginary)
+        else:
+            result = "%.2f-%.2fi" % (self.real, abs(self.imaginary))
+        return result
+
+if __name__ == '__main__':
