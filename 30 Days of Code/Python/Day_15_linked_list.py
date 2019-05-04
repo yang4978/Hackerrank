1a41ae1b4ab8631ae83de1a15f3d744ec68f5b48
yang4978 <40636563+yang4978@users.noreply.github.com>
2019-05-04 22:45:44 +0800

    Create Day_15_linked_list.py

---
+++ 30 Days of Code/Python/Day_15_linked_list.py
@@ -0,0 +1,32 @@
+#https://www.hackerrank.com/challenges/30-linked-list/problem
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def insert(self,head,data):
+        if (head == None):
+            head = Node(data);
+        else:
+            temp = head;
+            while(temp.next!=None):
+                temp =temp.next ;
+            temp.next = Node(data);
+            
+        return head;
+    #Complete this method
+
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)    
+mylist.display(head); 	  
