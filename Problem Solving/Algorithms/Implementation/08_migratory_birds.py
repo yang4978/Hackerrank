55e544971a2075f61652af59438baa1dcb0745fa
yang4978 <40636563+yang4978@users.noreply.github.com>
2019-05-17 11:52:12 +0200

    Create 08_migratory_birds.py

---
+++ Problem Solving/Algorithms/Implementation/08_migratory_birds.py
@@ -0,0 +1,30 @@
+#https://www.hackerrank.com/challenges/migratory-birds/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the migratoryBirds function below.
+def migratoryBirds(arr):
+    num_arr = [0,0,0,0,0,0];
+    for i in arr:
+        num_arr[i] += 1;
+    return num_arr.index(max(num_arr));
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
