311ca17f4afdf5a94fb74e292e8931baa370feec
yang4978 <40636563+yang4978@users.noreply.github.com>
2019-05-20 09:54:21 +0200

    Create 17_picking_numbers.py

---
+++ Problem Solving/Algorithms/Implementation/17_picking_numbers.py
@@ -0,0 +1,41 @@
+#https://www.hackerrank.com/challenges/picking-numbers/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pickingNumbers' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def pickingNumbers(a):
+    arr = set(a);
+    count = 0;
+    
+    for i in arr:
+        temp_plus = a.count(i)+a.count(i+1)
+        if (temp_plus>count):
+            count = temp_plus;
+    
+    return count;
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = pickingNumbers(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
