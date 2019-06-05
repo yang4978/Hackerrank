2105e0f0a843bfca0e2fa499dbea39c34e5971c7
yang4978 <40636563+yang4978@users.noreply.github.com>
2019-06-05 17:33:11 +0200

    Create 22_angry_professor.py

---
+++ Problem Solving/Algorithms/Implementation/22_angry_professor.py
@@ -0,0 +1,37 @@
+#https://www.hackerrank.com/challenges/angry-professor/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the angryProfessor function below.
+def angryProfessor(k, a):
+    num = sum([1 for i in a if i<=0]);
+    if(num-k>=0):
+        return 'NO'
+    else:
+        return 'YES'
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        nk = input().split()
+
+        n = int(nk[0])
+
+        k = int(nk[1])
+
+        a = list(map(int, input().rstrip().split()))
+
+        result = angryProfessor(k, a)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
