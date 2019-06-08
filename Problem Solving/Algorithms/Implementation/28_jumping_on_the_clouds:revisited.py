0b0f7120dbad86db34ae1c903210c330935d1909
yang4978 <40636563+yang4978@users.noreply.github.com>
2019-06-08 19:06:21 +0200

    Create 28_jumping_on_the_clouds:revisited.py

---
+++ Problem Solving/Algorithms/Implementation/28_jumping_on_the_clouds:revisited.py
@@ -0,0 +1,35 @@
+#https://www.hackerrank.com/challenges/jumping-on-the-clouds-revisited/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the jumpingOnClouds function below.
+def jumpingOnClouds(c, k):
+    e = 100;
+    n = len(c);
+    for i in range(0,n,k):
+        e -= 1+2*c[i];
+
+    return e
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
