26f32ae2da8f34d7a7215b053e1cf22c77b86bc2
yang4978 <40636563+yang4978@users.noreply.github.com>
2019-06-17 22:23:58 +0200

    Create 08_pangrams.py

---
+++ Problem Solving/Algorithms/Strings/08_pangrams.py
@@ -0,0 +1,27 @@
+#https://www.hackerrank.com/challenges/pangrams/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the pangrams function below.
+def pangrams(s):
+    for i in range(26):
+        if(chr(65+i) not in s and chr(97+i) not in s):
+            return 'not pangram'
+    return 'pangram'
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = pangrams(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
