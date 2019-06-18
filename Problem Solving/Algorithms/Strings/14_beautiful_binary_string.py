df54561838addb594f2fbb828535ba145ba418c5
yang4978 <40636563+yang4978@users.noreply.github.com>
2019-06-18 21:53:58 +0200

    Create 14_beautiful_binary_string.py

---
+++ Problem Solving/Algorithms/Strings/14_beautiful_binary_string.py
@@ -0,0 +1,27 @@
+#https://www.hackerrank.com/challenges/beautiful-binary-string/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the beautifulBinaryString function below.
+def beautifulBinaryString(b):
+    return b.count('010')
+    #return len(re.findall('010',b))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    b = input()
+
+    result = beautifulBinaryString(b)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
