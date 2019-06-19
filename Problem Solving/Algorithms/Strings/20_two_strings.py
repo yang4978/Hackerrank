aee7b636b73ce251810959b7b17bd31db7ca6ab8
yang4978 <40636563+yang4978@users.noreply.github.com>
2019-06-19 16:14:29 +0200

    Create 20_two_strings.py

---
+++ Problem Solving/Algorithms/Strings/20_two_strings.py
@@ -0,0 +1,30 @@
+#https://www.hackerrank.com/challenges/two-strings/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+# Complete the twoStrings function below.
+def twoStrings(s1, s2):
+    return 'YES' if sum((Counter(s1)&Counter(s2)).values()) else 'NO'
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        s1 = input()
+
+        s2 = input()
+
+        result = twoStrings(s1, s2)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
