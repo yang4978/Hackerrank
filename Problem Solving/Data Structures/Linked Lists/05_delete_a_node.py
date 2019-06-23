111e805c716061e19bf4cc67c6c8d92f69f2be5a
yang4978 <40636563+yang4978@users.noreply.github.com>
2019-06-23 11:37:08 +0200

    Create 05_delete_a_node.py

---
+++ Problem Solving/Data Structures/Linked Lists/05_delete_a_node.py
@@ -0,0 +1,80 @@
+#https://www.hackerrank.com/challenges/delete-a-node-from-a-linked-list/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the deleteNode function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def deleteNode(head, position):
+    if(position):
+        node = head
+        while(position-1):
+            node = node.next
+            position -= 1
+        node.next = node.next.next
+        return head
+    else:
+        return head.next
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for _ in range(llist_count):
+        llist_item = int(input())
+        llist.insert_node(llist_item)
+
+    position = int(input())
+
+    llist1 = deleteNode(llist.head, position)
+
+    print_singly_linked_list(llist1, ' ', fptr)
+    fptr.write('\n')
+
+    fptr.close()
