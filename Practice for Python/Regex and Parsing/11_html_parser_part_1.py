be0ff0295fa70216c0489a9d6ec86647728837a5
yang4978 <40636563+yang4978@users.noreply.github.com>
2019-06-30 19:29:38 +0200

    Create 11_html_parser_part_1.py

---
+++ Practice for Python/Regex and Parsing/11_html_parser_part_1.py
@@ -0,0 +1,22 @@
+#https://www.hackerrank.com/challenges/html-parser-part-1/problem
+
+from html.parser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print ("Start :", tag)
+        for i in attrs:
+            print('->',i[0],'>',i[1])
+    def handle_endtag(self, tag):
+        print ("End   :", tag)
+    def handle_startendtag(self, tag, attrs):
+        print ("Empty :", tag)
+        for i in attrs:
+            print('->',i[0],'>',i[1])
+
+
+parser = MyHTMLParser()
+
+for _ in range(int(input())):
+    parser.feed(input())
+
