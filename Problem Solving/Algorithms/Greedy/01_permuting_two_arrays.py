86db5b6b59d090fccccbe51389c5282217549569
yang4978 <40636563+yang4978@users.noreply.github.com>
2019-08-11 17:15:51 +0200

    Create 01_permuting_two_arrays.py

---
+++ Problem Solving/Algorithms/Greedy/01_permuting_two_arrays.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the twoArrays function below.
+def twoArrays(k, A, B):
+    A = sorted(A)
+    B = sorted(B)
+    B.reverse()
+    return "YES" if min([A[i]+B[i] for i in range(len(A))])>=k else "NO"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        nk = input().split()
+
+        n = int(nk[0])
+
+        k = int(nk[1])
+
+        A = list(map(int, input().rstrip().split()))
+
+        B = list(map(int, input().rstrip().split()))
+
+        result = twoArrays(k, A, B)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
